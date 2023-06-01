9dec78b23538e91a6196ab6063133dbc5451bae8
Kenpachi <ivandelyks228@gmail.com>
2023-06-02 02:18:13 +0300

    bug fix

--- src/main/java/ru/kazachkov/springboot/security/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.kazachkov.springboot.security.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.kazachkov.springboot.security.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
